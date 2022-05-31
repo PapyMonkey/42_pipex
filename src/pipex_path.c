87eba05b39d9fb77ff47a5d30a710871cc5b4115
Anthony Guiri <exastir@icloud.com>
2022-05-31 20:28:24 +0200

    fix(*): fix no PATH or ENV variable

--- src/pipex_path.c
+++ src/pipex_path.c
@@ -6,11 +6,25 @@
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 22:48:51 by aguiri            #+#    #+#             */
-/*   Updated: 2022/04/10 22:53:44 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/05/31 20:26:19 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static int	pipe_is_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH", 4) == 0)
+			return (EXIT_SUCCESS);
+		i++;
+	}
+	return (EXIT_FAILURE);
+}
 
 char	**ft_split_path(char **envp)
 {
@@ -19,8 +33,13 @@ char	**ft_split_path(char **envp)
 	char	**path_splitted;
 
 	i = 0;
-	while (ft_strncmp(envp[i], "PATH", 4) != 0 && envp[i] != NULL)
+	while (envp[i] != NULL && ft_strncmp(envp[i], "PATH", 4) != 0)
 		i++;
+	if (pipe_is_path(envp) == EXIT_FAILURE)
+	{
+		ft_printf("Error : no PATH variable\n");
+		exit (EXIT_FAILURE);
+	}
 	path_trimmed = ft_strtrim(envp[i], "PATH=");
 	path_splitted = ft_split(path_trimmed, ':');
 	free(path_trimmed);
--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 11:49:59 by aguiri            #+#    #+#             */
-/*   Updated: 2022/04/11 12:35:48 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/05/31 20:24:38 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ t_cmds	ft_pipex_construct(int argc, char **argv, char **envp)
 
 	out.args_nb = argc;
 	out.args = argv;
+	if (!envp)
+	{
+		ft_printf("Error : no environnement variable");
+		exit (EXIT_FAILURE);
+	}
 	out.envp = envp;
 	out.path = ft_split_path(out.envp);
 	out.pwd = ft_get_pwd(out.envp);
